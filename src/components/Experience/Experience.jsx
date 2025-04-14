9c4ff994ead93dedfc60032b73374b13c7555766
Dhruv Shah <69303943+DhruvShah28@users.noreply.github.com>
2025-04-14 11:57:01 -0400

    Update Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -9,7 +9,7 @@ export default function Experience() {
 
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch(`${url}/experiences/api/list`,{ mode: "no-cors" });
+            const response = await fetch(`${url}/experiences/api/list`);
             const data = await response.json();
             setExperience(data);
         }
